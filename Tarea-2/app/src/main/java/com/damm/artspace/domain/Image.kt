b5ff340608061123c9c29936f7929521baccfb36
diegoxze35 <diegoalv1217@gmail.com>
2025-09-19 02:09:50 -0600

    homework 2

---
+++ Tarea-2/app/src/main/java/com/damm/artspace/domain/Image.kt
@@ -0,0 +1,5 @@
+package com.damm.artspace.domain
+
+import android.net.Uri
+
+data class Image(val uri: Uri, val name: String)
---
+++ Tarea-2/app/src/main/java/com/damm/artspace/ui/navigation/route/Routes.kt
@@ -0,0 +1,9 @@
+package com.damm.artspace.ui.navigation.route
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object ImageGrid
+
+@Serializable
+object RationaleImages
